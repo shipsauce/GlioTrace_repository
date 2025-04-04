8f3ecfcdf78a4895fc226bb54f1af064496e3077
shipsauce <madeleineskeppas@gmail.com>
2025-04-04 14:19:44 +0200

    Changed indexing of table on row 73 and 84

--- build_cell_table.m
+++ build_cell_table.m
@@ -4,21 +4,26 @@ function tbl_out = build_cell_table(tbl_in)
 % (TAD) and the average speed across the track. The information on all
 % cells across all ROIs is saved in a new table where each row is a cell
 % and columns are variables containing the following:
-% set - set number
-% exp - experiment number
-% roi - roi number (in experiment)
-% hgcc - celline
-% perturbation - treatment/control
-% dose - dose of treatment
-% morphology - dominating morphology label of viterbi track
-% trax - x-coordinates of track
-% tray - y-coordinates of track
-% noisy_labels - raw morphology label sequence
-% viterbi_path - HMM corrected path
-% tme_labels - tme label sequence
-% tad - turning angle distribution (a vector for each subsequence)
-% dominating morph idx - indicies of subsequences
-% average speed - average speed in track (one value for each subsequence)
+%  set - set number
+%  exp - experiment number
+%  roi - roi number (in experiment)
+%  hgcc - celline
+%  perturbation - treatment/control
+%  dose - dose of treatment
+%  morphology - dominating morphology label of viterbi track
+%  tme - dominating tme label of raw track
+%  trax - x-coordinates of track
+%  tray - y-coordinates of track
+%  noisy_labels - raw morphology label sequence
+%  viterbi_path - HMM corrected path
+%  tme_labels - tme label sequence
+%  tad - turning angle distribution (a vector for each subsequence)
+%  tad_tme - same as above but calculated for track indices based on 
+%       dominating tme label
+%  dominating morph idx - indicies of subsequences
+%  average speed - average speed in track (one value for each subsequence)
+%  average speed_tme - same as above but calculated based on dominating tme
+%       label indices
 %
 % Input parameters:
 %   tbl_in - stacktable
@@ -31,8 +36,8 @@ function tbl_out = build_cell_table(tbl_in)
 
 % Define varnames of table
 varnames = {'set', 'exp', 'roi', 'hgcc', 'perturbation', 'dose', ...
-'morphology', 'trax', 'tray', 'noisy_labels', 'viterbi_path', ...
-'tme_labels', 'tad', 'dominating_morph_idx', 'average_speed'};
+'morphology', 'tme','trax', 'tray', 'noisy_labels', 'viterbi_path', ...
+'tme_labels', 'tad', 'tad_tme','dominating_morph_idx', 'average_speed', 'average_speed_tme'};
 
 tbl_out=table;
 
@@ -50,11 +55,13 @@ for i=1:height(tbl_in)
         path = paths{j};
         x = traxs(~isnan(traxs(:,j)),j);
         y = trays(~isnan(trays(:,j)),j);
-        morphology = mode(path); % Get dominating morph of path
+        morphology = mode(path); % Get dominating morphology of path
+        tme_dom = mode(props_tme(:,j)); % Get dominating TME association
 
         % Extract subsequences of the path where the label agrees with the
         % dominating label of the path
         idx = extractSubsequences(path, morphology);
+        idx2 = extractSubsequences(props_tme(~isnan(props_tme(:,j)),j), tme_dom);
         
         % Calculate TAD and speed for each of the subsequences
         tads = [];
@@ -63,11 +70,22 @@ for i=1:height(tbl_in)
             index = idx{m};
             [tad] = calculate_TAD(x(index), y(index));
             tads = [tads tad];
-            speed = calculate_cell_speed(x(index), y(index), tbl_in.delta_t(m));
+            speed = calculate_cell_speed(x(index), y(index), tbl_in.delta_t(i));
             speeds = [speeds; speed];
         end
 
-        tbl_out = [tbl_out; array2table({row.set, row.exp, row.roi, row.HGCC row.perturbation{:} row.dose morphology traxs(:,j) trays(:,j) props(:,j) path props_tme(:,j) tads' idx speeds},"VariableNames",varnames)];
+        % Do the same for dominating TME label
+        tads_tme = [];
+        speeds_tme = [];
+        for m=1:length(idx2)
+            index = idx2{m};
+            [tad] = calculate_TAD(x(index), y(index));
+            tads_tme = [tads_tme tad];
+            speed = calculate_cell_speed(x(index), y(index), tbl_in.delta_t(i));
+            speeds_tme = [speeds_tme; speed];
+        end
+
+        tbl_out = [tbl_out; array2table({row.set, row.exp, row.roi, row.HGCC row.perturbation{:} row.dose morphology tme_dom traxs(:,j) trays(:,j) props(:,j) path props_tme(:,j) tads' tads_tme' idx speeds speeds_tme},"VariableNames",varnames)];
     end
     i
 end
